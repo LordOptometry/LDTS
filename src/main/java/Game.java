83a094ba2d0efe5e23083974ced9abcc4680d14f
Nuno Silva <nunosilva@MacBook-Pro-de-Nuno-2.local>
2022-10-04 12:59:11 +0100

    dar um passo atrás para dar dois à frente

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -9,77 +8,33 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-
-    static TerminalScreen screen;
-    public Game() throws IOException {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+    private final TerminalScreen screen;
+    private int x = 10;
+    private int y = 10;
+    public Game(int  width, int height) throws IOException {
+        // Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
         screen = new TerminalScreen(terminal);
-        TerminalSize terminalSize = new TerminalSize(40, 20);
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-        terminal = terminalFactory.createTerminal();
+        screen.setCursorPosition(null);   // we don’t need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+        TerminalSize terminalSize = new TerminalSize(width, height);
+        // DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
     }
-
-
-    static void run(Game game) throws IOException {
-
-        draw();
-
-        KeyStroke key = screen.readInput();
-
-        while (true) {
-            if (key.getKeyType() == KeyType.EOF) {
-                break;
-            }
-        }
-
-        processKey(key);
-    }
-
-    private static void draw() throws IOException{
-
+    private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(hero.getX(), hero.getY(), TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
+    public void run() throws IOException {
+        while (true){
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+        }
+    }
 
-
-
-
-    static Position position = new Position(10,10);
-    static Hero hero  = new Hero(position);
-
-
-
-
-    private static void processKey(KeyStroke key) {
-
+    private void processKey(KeyStroke key){
         System.out.println(key);
-
-
-        if(key.getKeyType() == KeyType.ArrowUp){
-            screen.setCharacter(hero.getX(), hero.getY() + 1, TextCharacter.fromCharacter('X')[0]);
-        }
-        if(key.getKeyType() == KeyType.ArrowDown){
-            screen.setCharacter(hero.getX(), hero.getY() - 1, TextCharacter.fromCharacter('X')[0]);
-        }
-        if(key.getKeyType() == KeyType.ArrowRight){
-            screen.setCharacter(hero.getX() + 1, hero.getY(), TextCharacter.fromCharacter('X')[0]);
-        }
-        if(key.getKeyType() == KeyType.ArrowLeft){
-            screen.setCharacter(hero.getX() - 1, hero.getY() , TextCharacter.fromCharacter('X')[0]);
-        }
-        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
-            screen.clear();
-        }
-
     }
-
-    KeyStroke readInput() {
-        return null;
-    }
-
-
-
-
-
 }
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,6 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+
 public class Hero {
 
     private Position position;
@@ -6,10 +9,13 @@ public class Hero {
         this.position = position;
     }
 
+    public void draw(Screen screen){
+        screen.setCharacter(position.getx(), position.gety(), TextCharacter.fromCharacter('X')[0]);
+    }
     public int getX(){return position.getx();}
     public int getY(){return position.gety();}
 
     public void setX(int x){position.setx(x);}
-    public void setY(int y){position.setx(y);}
+    public void setY(int y){position.sety(y);}
 
 }
