328acab1526a5bc140b42db85f955af14c8495f0
Nuno Silva <nunosilva@MacBook-Pro-de-Nuno-2.local>
2022-10-04 11:57:35 +0100

    better position

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,72 +1,85 @@
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
 public class Game {
-    public static void main(String[] args) throws IOException {
-        Game game = new Game();
-        Screen.run(game);
+
+    static TerminalScreen screen;
+    public Game() throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        screen = new TerminalScreen(terminal);
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        terminal = terminalFactory.createTerminal();
     }
-    public interface Screen{
-        Screen screen = null;
 
-        Position position = new Position(10,10);
-        Hero hero  = new Hero(position);
 
-        private static void draw() throws IOException{
+    static void run(Game game) throws IOException {
 
+        draw();
+
+        KeyStroke key = screen.readInput();
+
+        while (true) {
+            if (key.getKeyType() == KeyType.EOF) {
+                break;
+            }
+        }
+
+        processKey(key);
+    }
+
+    private static void draw() throws IOException{
+
+        screen.clear();
+        screen.setCharacter(hero.getX(), hero.getY(), TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+
+
+
+    static Position position = new Position(10,10);
+    static Hero hero  = new Hero(position);
+
+
+
+
+    private static void processKey(KeyStroke key) {
+
+        System.out.println(key);
+
+
+        if(key.getKeyType() == KeyType.ArrowUp){
+            screen.setCharacter(hero.getX(), hero.getY() + 1, TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowDown){
+            screen.setCharacter(hero.getX(), hero.getY() - 1, TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowRight){
+            screen.setCharacter(hero.getX() + 1, hero.getY(), TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowLeft){
+            screen.setCharacter(hero.getX() - 1, hero.getY() , TextCharacter.fromCharacter('X')[0]);
+        }
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
             screen.clear();
-            screen.setCharacter(hero.getX(), hero.getY(), TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-        }
-
-
-        private static void processKey(KeyStroke key) {
-
-            System.out.println(key);
-
-
-            if(key.getKeyType() == KeyType.ArrowUp){
-                screen.setCharacter(hero.getX(), hero.getY() + 1, TextCharacter.fromCharacter('X')[0]);
-            }
-            if(key.getKeyType() == KeyType.ArrowDown){
-                screen.setCharacter(hero.getX(), hero.getY() - 1, TextCharacter.fromCharacter('X')[0]);
-            }
-            if(key.getKeyType() == KeyType.ArrowRight){
-                screen.setCharacter(hero.getX() + 1, hero.getY(), TextCharacter.fromCharacter('X')[0]);
-            }
-            if(key.getKeyType() == KeyType.ArrowLeft){
-                screen.setCharacter(hero.getX() - 1, hero.getY() , TextCharacter.fromCharacter('X')[0]);
-            }
-            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
-                screen.clear();
-            }
-
-        }
-
-        KeyStroke readInput();
-
-        void setCharacter(int i, int i1, TextCharacter x);
-
-        void refresh();
-
-        void clear();
-
-        static void run(Game game) throws IOException {
-            draw();
-
-            KeyStroke key = screen.readInput();
-
-            while (true) {
-                if (key.getKeyType() == KeyType.EOF) {
-                    break;
-                }
-            }
-
-            processKey(key);
         }
 
     }
+
+    KeyStroke readInput() {
+        return null;
+    }
+
+
+
+
+
 }
