3adf9c1babb136a3b5c7293fd4850904b98b18fb
Nuno Silva <nunosilva@MacBook-Pro-de-Nuno-2.local>
2022-10-04 15:37:33 +0100

    X a andar

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -12,14 +13,13 @@ public class Game {
     private int x = 10;
     private int y = 10;
     public Game(int  width, int height) throws IOException {
-        // Terminal terminal = new DefaultTerminalFactory().createTerminal();
+
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
         screen = new TerminalScreen(terminal);
         screen.setCursorPosition(null);   // we don’t need a cursor
         screen.startScreen();             // screens must be started
         screen.doResizeIfNecessary();     // resize screen if necessary
         TerminalSize terminalSize = new TerminalSize(width, height);
-        // DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
     }
     private void draw() throws IOException {
         screen.clear();
@@ -31,10 +31,32 @@ public class Game {
             draw();
             KeyStroke key = screen.readInput();
             processKey(key);
+            if(key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                screen.close();
+            }
+            if(key.getKeyType() == KeyType.EOF){
+                break;
+            }
         }
     }
 
-    private void processKey(KeyStroke key){
+    private void processKey(KeyStroke key) throws IOException {
         System.out.println(key);
+        if(key.getKeyType() == KeyType.ArrowUp){
+            y--;
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowDown){
+            y++;
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowLeft){
+            x--;
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        }
+        if(key.getKeyType() == KeyType.ArrowRight){
+            x++;
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        }
     }
 }
